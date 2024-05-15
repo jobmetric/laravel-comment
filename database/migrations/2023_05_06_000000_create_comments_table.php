6aedd208cac64803d2c3e1783eb52705b3358bd3
majid mohammadian <majeedmohammadian@gmail.com>
2024-05-15 21:19:44 +0330

    add migration file

---
+++ database/migrations/2023_05_06_000000_create_comments_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create(config('comment.tables.comment'), function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')->index()->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('parent_id')->nullable()->index()->constrained(config('comment.tables.comment'))->cascadeOnUpdate()->cascadeOnDelete();
+
+            $table->morphs('commentable');
+
+            $table->longText('text')->nullable();
+
+            $table->dateTime('published_at')->nullable()->index();
+            $table->foreignId('published_by')->index()->constrained(config('comment.tables.user'))->cascadeOnUpdate()->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+
+        cache()->forget('comments');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(config('comment.tables.comment'));
+
+        cache()->forget('comments');
+    }
+};
