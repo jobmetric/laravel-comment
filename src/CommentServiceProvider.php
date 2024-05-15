b14594720f6e7002aa7b1eade8e46eac47608ac2
majid mohammadian <majeedmohammadian@gmail.com>
2024-05-15 22:03:36 +0330

    add service provider

---
+++ src/CommentServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace JobMetric\Comment;
+
+use JobMetric\PackageCore\Exceptions\MigrationFolderNotFoundException;
+use JobMetric\PackageCore\PackageCore;
+use JobMetric\PackageCore\PackageCoreServiceProvider;
+
+class CommentServiceProvider extends PackageCoreServiceProvider
+{
+    /**
+     * @throws MigrationFolderNotFoundException
+     */
+    public function configuration(PackageCore $package): void
+    {
+        $package->name('laravel-comment')
+            ->hasConfig()
+            ->hasMigration();
+    }
+}
