9f05ee2ed0e1b8bf244eba465dba835b94586b83
majid mohammadian <majeedmohammadian@gmail.com>
2024-06-18 22:47:22 +0330

    change sotre comment in trait

--- src/HasComment.php
+++ src/HasComment.php
@@ -8,6 +8,7 @@ use JobMetric\Comment\Events\CommentForgetEvent;
 use JobMetric\Comment\Events\CommentStoredEvent;
 use JobMetric\Comment\Events\CommentUpdateEvent;
 use JobMetric\Comment\Exceptions\ModelCommentContractNotFoundException;
+use JobMetric\Comment\Http\Resources\CommentResource;
 use JobMetric\Comment\Models\Comment;
 use Throwable;
 
@@ -51,80 +52,16 @@ trait HasComment
     }
 
     /**
-     * store comment
-     *
-     * @param string $text
-     * @param int $parent_id
-     * @param int|null $user_id
-     *
-     * @return array
-     * @throws Throwable
-     */
-    public function comment(string $text, int $parent_id = 0, int $user_id = null): array
-    {
-        $comment = $this->commentTo()->create([
-            'user_id' => $user_id,
-            'text' => $text,
-            'parent_id' => $parent_id
-        ]);
-
-        event(new CommentStoredEvent($comment));
-
-        return [
-            'ok' => true,
-            'message' => trans('comment::base.messages.created'),
-            'data' => $comment,
-            'status' => 201
-        ];
-    }
-
-    /**
-     * update comment
-     *
-     * @param int $comment_id
-     * @param string $text
-     *
-     * @return array
-     * @throws Throwable
-     */
-    public function updateComment(int $comment_id, string $text): array
-    {
-        /** @var Comment $comment */
-        $comment = $this->commentTo()->where('id', $comment_id)->first();
-
-        if ($comment) {
-            $comment->update([
-                'text' => $text
-            ]);
-
-            event(new CommentUpdateEvent($comment));
-
-            return [
-                'ok' => true,
-                'message' => trans('comment::base.messages.updated'),
-                'data' => $comment,
-                'status' => 200
-            ];
-        }
-
-        return [
-            'ok' => false,
-            'message' => trans('comment::base.messages.updated'),
-            'status' => 404
-        ];
-    }
-
-    /**
      * store comment by user
      *
      * @param int $user_id
      * @param string $text
-     * @param int $parent_id
+     * @param int|null $parent_id
      *
      * @return array
      * @throws Throwable
      */
-    public function commentBy(int $user_id, string $text, int $parent_id = 0): array
+    public function commentBy(int $user_id, string $text, int $parent_id = null): array
     {
         return $this->comment($text, $parent_id, $user_id);
     }
@@ -183,7 +120,7 @@ trait HasComment
      *
      * @return int|null
      */
-    public function isCommentedStatusBy(int $user_id): ?int
+    public function isCommentedByUser(int $user_id): ?int
     {
         /* @var Comment $comment */
         $comment = $this->commentTo()->where('user_id', $user_id)->first();
@@ -192,24 +129,101 @@ trait HasComment
     }
 
     /**
-     * forget comment
+     * store comment
      *
-     * @param int $user_id
+     * @param string $text
+     * @param int|null $parent_id
+     * @param int|null $user_id
      *
-     * @return static
+     * @return array
+     * @throws Throwable
      */
-    public function forgetComment(int $user_id): static
+    public function comment(string $text, int $parent_id = null, int $user_id = null): array
     {
-        /* @var Comment $comment */
-        $comment = $this->commentTo()->where('user_id', $user_id)->first();
+        $comment = $this->commentTo()->create([
+            'user_id' => $user_id,
+            'parent_id' => $parent_id,
+            'text' => $text
+        ]);
 
-        if ($comment) {
-            $comment->delete();
+        event(new CommentStoredEvent($comment));
 
-            event(new CommentForgetEvent($comment));
+        return [
+            'ok' => true,
+            'message' => trans('comment::base.messages.created'),
+            'data' => CommentResource::make($comment),
+            'status' => 201
+        ];
+    }
+
+    /**
+     * update comment
+     *
+     * @param int $comment_id
+     * @param string $text
+     *
+     * @return array
+     * @throws Throwable
+     */
+    public function updateComment(int $comment_id, string $text): array
+    {
+        /** @var Comment $comment */
+        $comment = $this->commentTo()->where('id', $comment_id)->first();
+
+        if (!$comment) {
+            return [
+                'ok' => false,
+                'message' => trans('comment::base.validation.not_found'),
+                'status' => 404
+            ];
         }
 
-        return $this;
+        $comment->update([
+            'text' => $text
+        ]);
+
+        event(new CommentUpdateEvent($comment));
+
+        return [
+            'ok' => true,
+            'message' => trans('comment::base.messages.updated'),
+            'data' => CommentResource::make($comment),
+            'status' => 200
+        ];
+    }
+
+    /**
+     * forget comment
+     *
+     * @param int $comment_id
+     *
+     * @return array
+     */
+    public function forgetComment(int $comment_id): array
+    {
+        /* @var Comment $comment */
+        $comment = $this->commentTo()->where('id', $comment_id)->first();
+
+        if (!$comment) {
+            return [
+                'ok' => false,
+                'message' => trans('comment::base.validation.not_found'),
+                'status' => 404
+            ];
+        }
+
+        $data = CommentResource::make($comment);
+
+        $comment->delete();
+
+        event(new CommentForgetEvent($comment));
+
+        return [
+            'ok' => true,
+            'message' => trans('comment::base.messages.deleted'),
+            'data' => $data,
+            'status' => 200
+        ];
     }
 
     /**
@@ -227,5 +241,40 @@ trait HasComment
         });
 
         return $this;
+    }
+
+    /**
+     * approve comment
+     *
+     * @param int $comment_id
+     * @param int|null $user_id
+     *
+     * @return array
+     * @throws Throwable
+     */
+    public function approveComment(int $comment_id, int $user_id = null): array
+    {
+        /** @var Comment $comment */
+        $comment = $this->commentTo()->where('id', $comment_id)->first();
+
+        if (!$comment) {
+            return [
+                'ok' => false,
+                'message' => trans('comment::base.validation.not_found'),
+                'status' => 404
+            ];
+        }
+
+        $comment->update([
+            'approved_at' => now(),
+            'approved_by' => $user_id ?? auth()->id()
+        ]);
+
+        return [
+            'ok' => true,
+            'message' => trans('comment::base.messages.approved'),
+            'data' => CommentResource::make($comment),
+            'status' => 200
+        ];
     }
 }
