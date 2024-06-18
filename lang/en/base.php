4b8dac57dc19d1e4f26de20847b1e78a5db5bc4f
majid mohammadian <majeedmohammadian@gmail.com>
2024-06-18 22:47:58 +0330

    add another language

--- lang/en/base.php
+++ lang/en/base.php
@@ -21,6 +21,12 @@ return [
         'created' => 'The comment was created successfully.',
         'updated' => 'The comment was updated successfully.',
         'deleted' => 'The comment was deleted successfully.',
+        'approved' => 'The comment was approved successfully.',
+    ],
+
+    'exceptions' => [
+        'model_has_comment_not_found' => 'Model :model not use JobMetric\Comment\HasComment Trait!',
+        'model_comment_contract_not_found' => 'Model :model not implements JobMetric\Comment\Contracts\CommentContract interface!',
     ],
 
 ];
