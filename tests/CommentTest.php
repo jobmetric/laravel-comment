a39a8747802bb54c9bdd0d13a0068ceeadf8893d
majid mohammadian <majeedmohammadian@gmail.com>
2024-06-18 22:48:39 +0330

    change test file

--- tests/CommentTest.php
+++ tests/CommentTest.php
@@ -32,4 +32,91 @@ class CommentTest extends BaseTestCase
             'text' => 'test comment',
         ]);
     }
+
+    /**
+     * @throws Throwable
+     */
+    public function testUpdate(): void
+    {
+        // store product
+        /** @var Product $product */
+        $product = Product::create([
+            'status' => true,
+        ]);
+
+        $comment = $product->comment('test comment');
+
+        $updatedComment = $product->updateComment($comment['data']->id, 'updated comment');
+
+        $this->assertIsArray($updatedComment);
+        $this->assertTrue($updatedComment['ok']);
+        $this->assertEquals(200, $updatedComment['status']);
+        $this->assertIsInt($updatedComment['data']->id);
+        $this->assertDatabaseHas('comments', [
+            'id' => $updatedComment['data']->id,
+            'commentable_id' => $product->id,
+            'commentable_type' => Product::class,
+            'text' => 'updated comment',
+        ]);
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testForget(): void
+    {
+        // store product
+        /** @var Product $product */
+        $product = Product::create([
+            'status' => true,
+        ]);
+
+        $comment = $product->comment('test comment');
+
+        $deleteComment = $product->forgetComment($comment['data']->id);
+
+        $this->assertIsArray($deleteComment);
+        $this->assertTrue($deleteComment['ok']);
+        $this->assertEquals(200, $deleteComment['status']);
+        $this->assertDatabaseMissing('comments', [
+            'id' => $comment['data']->id,
+            'commentable_id' => $product->id,
+            'commentable_type' => Product::class,
+            'text' => 'test comment',
+        ]);
+
+        // double delete
+        $deleteComment = $product->forgetComment($comment['data']->id);
+
+        $this->assertIsArray($deleteComment);
+        $this->assertFalse($deleteComment['ok']);
+        $this->assertEquals(404, $deleteComment['status']);
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testApprove(): void
+    {
+        // store product
+        /** @var Product $product */
+        $product = Product::create([
+            'status' => true,
+        ]);
+
+        $comment = $product->comment('test comment');
+
+        $approveComment = $product->approveComment($comment['data']->id);
+
+        $this->assertIsArray($approveComment);
+        $this->assertTrue($approveComment['ok']);
+        $this->assertEquals(200, $approveComment['status']);
+        $this->assertDatabaseHas('comments', [
+            'id' => $comment['data']->id,
+            'commentable_id' => $product->id,
+            'commentable_type' => Product::class,
+            'text' => 'test comment',
+            'approved_at' => now(),
+        ]);
+    }
 }
