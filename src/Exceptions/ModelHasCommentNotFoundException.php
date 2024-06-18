d3e024adea2621e3453299c7923d51382b6fa5d5
majid mohammadian <majeedmohammadian@gmail.com>
2024-06-18 22:46:03 +0330

    add translation to exception

--- src/Exceptions/ModelHasCommentNotFoundException.php
+++ src/Exceptions/ModelHasCommentNotFoundException.php
@@ -5,11 +5,11 @@ namespace JobMetric\Comment\Exceptions;
 use Exception;
 use Throwable;
 
-class ModelCommentContractNotFoundException extends Exception
+class ModelHasCommentNotFoundException extends Exception
 {
     public function __construct(string $model, int $code = 400, ?Throwable $previous = null)
     {
-        parent::__construct(trans('comment::base.exceptions.model_comment_contract_not_found', [
+        parent::__construct(trans('comment::base.exceptions.model_has_comment_not_found', [
             'model' => $model
         ]), $code, $previous);
     }
